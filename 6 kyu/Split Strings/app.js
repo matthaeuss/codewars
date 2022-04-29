fdab95eea9de8fae8284e966404ea56ac9ea74cc
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-04-29 11:01:17 +0200

    split_strings kata's solution added

---
+++ 6 kyu/Split Strings/app.js
@@ -0,0 +1,12 @@
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+//
+// Examples:
+//
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+function solution(str){
+    return (str.length % 2 ? str + '_' : str).match(/../gm) || []
+}
+
+solution('abc');
