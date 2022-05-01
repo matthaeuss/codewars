e28f4adc89a37a58a7ee41cb479e5e74f1e12be0
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-05-01 15:19:35 +0200

    sorting_with_a_sorting_array kata's solution added

---
+++ 7 kyu/Sort with a sorting array/app.js
@@ -0,0 +1,18 @@
+//Sort an array according to the indices in another array. It is guaranteed that the two arrays have the same size, and that the sorting array has all the required indices.
+//
+// initialArray = ['x', 'y', 'z'] sortingArray = [ 1, 2, 0]
+//
+// sort(initialArray, sortingArray) => ['z', 'x', 'y']
+//
+// sort(['z', 'x', 'y'], [0, 2, 1]) => ['z', 'y', 'x']
+
+function sort(initialArray, sortingArray) {
+    const temp = []
+    for (let i = 0; i < sortingArray.length; i++){
+        temp[sortingArray[i]] = initialArray[i];
+    }
+    return temp;
+}
+
+sort(['x', 'y', 'z'], [ 1, 2, 0]);
+
