93bee8a165e58a922111afa30975b97bf5637158
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-05-03 10:06:05 +0200

    simple_fun kata's solution added

---
+++ 7 kyu/Simple Fun #144: Distinct Digit Year/app.js
@@ -0,0 +1,20 @@
+//The year of 2013 is the first year after the old 1987 with only distinct digits.
+//
+// Now your task is to solve the following problem: given a year number, find the minimum year number which is strictly larger than the given one and has only distinct digits.
+//
+// Input/Output
+// [input] integer year
+// 1000 ≤ year ≤ 9000
+//
+// [output] an integer
+// the minimum year number that is strictly larger than the input number year and all its digits are distinct.
+
+
+function distinctDigitYear(year) {
+    do {
+        year++;
+    } while(new Set(`${year}`).size < 4);
+    return year;
+}
+
+distinctDigitYear(1993);
