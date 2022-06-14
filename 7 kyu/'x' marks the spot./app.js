7aa2ab7b8986f26f604e1d3469679c0795fcfb7d
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-06-14 12:15:05 +0200

    kata's solution added

---
+++ 7 kyu/'x' marks the spot./app.js
@@ -0,0 +1,56 @@
+// #'x' marks the spot
+
+// ##Task:
+
+// Given a two dimensional array, return the co-ordinates of 'x'.
+
+// If 'x' is not inside the array, or if 'x' appears multiple times, return []
+
+// The co-ordinates should be zero indexed.
+
+// You should assume you will always get an array as input. The array will only contain 'x's and 'o's.
+
+// Example test cases:
+
+// 'Return an empty array if input is an empty array' => []
+
+// [] 
+
+// 'Return an empty array if no x found' => []
+
+// [
+//   ['o', 'o'],
+//   ['o', 'o']
+// ]
+
+// 'Return an empty array if more than one x found' => []
+
+// [
+//   ['x', 'o'],
+//   ['o', 'x']
+// ]
+
+// 'Return [0,0] when x at top left' => [0, 0]
+
+// [
+//   ['x', 'o'],
+//   ['o', 'o']
+// ]
+
+// 'Return [4,6] for the example below' => [4, 6]
+
+// [
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'x', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
+// ]
+
+const xMarksTheSpot = (input) => {
+    const xs = input.filter(e => e.filter(e => e.indexOf('x') !== -1).length);
+    if (xs.length !== 1 || (xs.length === 1  && xs[0].filter(e => e === 'x').length !== 1)) return []
+    
+    return input.map((e, idx) => e.map((x, idx2) => x === 'x' ? [idx, idx2] : '').filter(e => e !== '')).filter(e => e.length > 0)[0][0]
+  }
