80d567c1fd35a9b5a56ab6f34ac0e3d64baf8c26
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-06-20 10:27:53 +0200

    kata's solution added

---
+++ 8 kyu/Squash the bugs/app.js
@@ -0,0 +1,9 @@
+// Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+
+// There will only be one 'longest' word.
+
+
+function findLongest(str) {
+ 
+    return Math.max(...str.split(" ").map(a=>a.length))
+   }
