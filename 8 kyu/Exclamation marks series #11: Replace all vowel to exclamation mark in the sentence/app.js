e91ccd0e8353286a5aff4c33fd937929a7e73665
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-06-23 12:57:08 +0200

    kata's solution added

---
+++ 8 kyu/Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence/app.js
@@ -0,0 +1,12 @@
+// Description:
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+// Examples
+// replace("Hi!") === "H!!"
+// replace("!Hi! Hi!") === "!H!! H!!"
+// replace("aeiou") === "!!!!!"
+// replace("ABCDE") === "!BCD!"
+
+function replace(str){
+    return str.replace(/[a,e,i,o,u,A,E,I,O,U]/gi, '!');   
+   }
